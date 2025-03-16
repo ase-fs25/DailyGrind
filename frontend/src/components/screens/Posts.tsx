d1d5a45acda2e35f9caa2f89e3e2c9a3e20b6da8
jnsgbl <40355159+jnsgbl@users.noreply.github.com>
2025-03-16 13:23:40 +0100

    Initial commit with basic structure (#28)
    Co-authored-by: Jonas Gebel <jonas.gebel@bf.uzh.ch>

---
+++ frontend/src/components/screens/Posts.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+import Header from "../common/Header";
+
+import "../../styles/components/screens/screen.css";
+
+const Posts = () => {
+  return (
+    <Box className="screen-container">
+      <Header />
+      <Box className="screen-content">
+        <Typography variant="h4">This is the Posts page</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Posts;
