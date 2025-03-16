69766fa2f17c11a3e348bb0cc0067ba8043d545f
Tony Krstic <toni2002krstic@gmail.com>
2025-03-16 23:38:29 +0100

    added missing data

--- ms-frontend/src/components/screens/Friends.tsx
+++ ms-frontend/src/components/screens/Friends.tsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card, CardContent, Avatar } from "@mui/material";
 import Header from "../common/Header";
+import { mockFriends } from "../../mockData/mockFriends";
 
 import "../../styles/components/screens/screen.css";
 
@@ -7,8 +8,32 @@ const Friends = () => {
   return (
     <Box className="screen-container">
       <Header />
-      <Box className="screen-content">
-        <Typography variant="h4">This is the Friends page</Typography>
+      <Box className="screen-content" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
+          Friends
+        </Typography>
+        {mockFriends.map((friend) => (
+          <Card
+            key={friend.userId}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: 600,
+              mb: 2,
+              p: 1,
+              boxShadow: 3,
+            }}
+          >
+            <Avatar
+              src={friend.profilePicture}
+              sx={{ width: 64, height: 64, ml: 2, mr: 2 }}
+            />
+            <CardContent>
+              <Typography variant="h6">{friend.username}</Typography>
+            </CardContent>
+          </Card>
+        ))}
       </Box>
     </Box>
   );
