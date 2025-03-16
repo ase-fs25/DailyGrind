a9e126f7faecc7021c5af1df03c91b89174c08f6
Mete Polat <81754505+polatmete@users.noreply.github.com>
2025-03-17 00:40:42 +0100

    Feature/frontend cicd pipeline (#40)
    * Add .gitignore
    
    * Solve dependency issues
    
    * Add prettier command
    
    * Run prettier command
    
    * Disable npm test and npm audit temporarily

--- ms-frontend/src/index.tsx
+++ ms-frontend/src/index.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
-import "./styles/index.css";
-import App from "./App";
+import './styles/index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
