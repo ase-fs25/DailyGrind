099a38963aea863f3cbe31c7684eec3e0a452af2
Tim <tim.vorburger@uzh.ch>
2025-03-18 19:28:56 +0100

    added mockVapidKeys.ts (should be stored in backend)

---
+++ ms-frontend/src/mockData/mockVapidKeys.ts
@@ -0,0 +1,5 @@
+export const mockVapidKeys = {
+    publicKey:
+'BGNKMIqVDc7udZPZ8manv9UF7uzQtCaYJvzEEe7rr6zor3HPkFuPTN5q1cUoABwYR-Dwa5Fwhx0BUOImZJC-rG8',
+    privateKey: '1tyL-Kw64dsVRGE7uUew77koormjIC67lu-jbTLWW0k'
+}
