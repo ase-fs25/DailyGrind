14202e8af4fcf98de7f75c764e513c7223a222df
Tim <tim.vorburger@uzh.ch>
2025-03-18 13:42:17 +0100

    added ServiceWorker

---
+++ ms-frontend/public/serviceWorker.ts
@@ -0,0 +1,17 @@
+declare const self: ServiceWorkerGlobalScope;
+
+self.addEventListener('push', (event: PushEvent) => {
+    const data = event.data ? event.data.text() : 'This is a push notification';
+
+    const options: NotificationOptions = {
+        body: data,
+        icon: 'https://via.placeholder.com/150',
+        badge: 'https://via.placeholder.com/150'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
+
+export {};
