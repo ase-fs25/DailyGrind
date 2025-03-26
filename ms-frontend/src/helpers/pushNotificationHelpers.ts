72b2bf3ea6febe6b134f059aefcf539ffd3d836c
Tim <tim.vorburger@uzh.ch>
2025-03-26 09:53:49 +0100

    added testPushNotification.js

--- ms-frontend/src/helpers/pushNotificationHelpers.ts
+++ ms-frontend/src/helpers/pushNotificationHelpers.ts
@@ -32,9 +32,42 @@ export const subscribeUserToPush = async () => {
             };
             subscription = await registration.pushManager.subscribe(subscribeOptions);
             console.log('Created new PushSubscription: ', JSON.stringify(subscription));
+
+            /*
+            if (subscription) {
+                await sendSubscriptionToBackend(subscription);
+            }
+
+             */
         }
         return subscription;
     } catch (error) {
         console.error('Error subscribing to push:', error);
     }
 };
+
+/*
+ * TODO: Implement the Endpoint in the backend
+export const sendSubscriptionToBackend = async (subscription: PushSubscription): Promise<boolean> => {
+    try {
+        const response = await fetch('API_ENDPOINT/subscribe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(subscription),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to send subscription to server');
+        }
+
+        console.log('Successfully sent subscription to server');
+        return true;
+    } catch (error) {
+        console.error('Error sending subscription to server:', error);
+        return false;
+    }
+};
+
+ */
