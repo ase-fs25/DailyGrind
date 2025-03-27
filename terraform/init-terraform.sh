8f0a1b3aa6180a151b0687e53a0ca3f7c807c98f
jnsgbl <40355159+jnsgbl@users.noreply.github.com>
2025-03-27 14:56:01 +0100

    Add changes to get terraform script running for Apple M1 (#68)
    Co-authored-by: Jonas Gebel <jonas.gebel@bf.uzh.ch>

--- terraform/init-terraform.sh
+++ terraform/init-terraform.sh
@@ -3,6 +3,12 @@
 # Remove the localstack_providers_override.tf file if it exists
 rm -f localstack_providers_override.tf
 
+# Use this line if you are running on Apple M- chip (tested for M1 chip)
+# export GODEBUG=asyncpreemptoff=1
+
+# Use this line to see logs while running the init command
+# export TF_LOG=DEBUG
+
 # Initialize and apply Terraform with LocalStack
 tflocal init
 tflocal apply -auto-approve
