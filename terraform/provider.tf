1ff49945a7ca53d1bf36f4e90c54deb6ed8272e8
lwagner <leowa444@gmail.com>
2025-03-27 16:24:04 +0100

    restructured project and terraform can now run in the container 🥳

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,12 @@
-provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "us-east-1"
-
-  endpoints {
-    cognitoidp = "http://locahost:4566"
-  }
-}
+# provider "aws" {
+#   access_key = "test"
+#   secret_key = "test"
+#   region     = "us-east-1"
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   skip_requesting_account_id  = true
+#
+#   endpoints {
+#     cognitoidp = "http://locahost:4566"
+#   }
+# }
