a29d9dd4bd71dcff16cd3f5bedfeac16c7a850d2
Tim <tim.vorburger@uzh.ch>
2025-04-08 16:35:44 +0200

    amended folder structure

---
+++ microservices/push-notification-service/src/main/java/com/uzh/ase/dailygrind/pushnotificationsservice/MsPushNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.uzh.ase.dailygrind.mspushnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPushNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPushNotificationsApplication.class, args);
+    }
+
+}
