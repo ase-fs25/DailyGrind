4dea58fab6f1db2ef65a2ce5bbbcf2477b045b3a
Mete Polat <81754505+polatmete@users.noreply.github.com>
2025-04-28 10:27:45 +0200

    Feature/localstack deployment (#98)
    * Containerization of everything and api gateway V1
    
    * docker-compose.yaml: Same network for every service + print api gateway urls
    
    * frontend deployment to S3
    
    * Add authorization to api gateways
    
    * Adjust docker compose profiles

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -7,8 +7,8 @@ resource "aws_cognito_user_pool" "daily_grind_user_pool" {
 
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
-    email_subject = "Your verification code"
-    email_message = "Your verification code is {####}"
+    email_subject        = "Your verification code"
+    email_message        = "Your verification code is {####}"
   }
 
   // Lambda function to automatically confirm users without needing to verify their email
@@ -28,14 +28,14 @@ resource "aws_cognito_user_pool" "daily_grind_user_pool" {
 
 # Cognito Client
 resource "aws_cognito_user_pool_client" "daily_grind_app_client" {
-  name = "daily-grind-app-client"
-  user_pool_id = aws_cognito_user_pool.daily_grind_user_pool.id
+  name                                 = "daily-grind-app-client"
+  user_pool_id                         = aws_cognito_user_pool.daily_grind_user_pool.id
   allowed_oauth_scopes = ["email", "openid", "profile"]
   allowed_oauth_flows_user_pool_client = true
-  generate_secret = true
+  generate_secret                      = true
   allowed_oauth_flows = ["code", "implicit"]
-  callback_urls = ["http://localhost:3000", "https://oauth.pstmn.io/v1/callback"]
-  logout_urls = ["http://localhost:3000"]
+  callback_urls = ["http://localhost:3000", "http://localhost:4566", "https://oauth.pstmn.io/v1/callback"]
+  logout_urls = ["http://localhost:3000", "http://localhost:4566"]
   supported_identity_providers = ["COGNITO"]
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
