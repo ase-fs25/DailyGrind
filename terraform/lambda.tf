858ad8c9716b35b077c55e7a8a2884c1d2673e0d
lwagner <leowa444@gmail.com>
2025-04-28 11:15:30 +0200

    fixed bugs

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -25,7 +25,7 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "confirm_user_lambda" {
   filename         = "./../lambda-functions/user-confirmation-lambda.zip"
   function_name    = "userConfirmationLambda"
-  role             = aws_iam_role.iam_for_ecs.arn
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "handler.lambda_handler"
   runtime          = "python3.8"
   source_code_hash = data.archive_file.lambda.output_base64sha256
